61b20e1e269c129cebf471db38b1c9a6060c8473
Archana Kalathiya <94705812+ArchanaKalathiya@users.noreply.github.com>
2023-12-21 20:04:05 +0530

    Default Constructor

---
+++ src/myPackage/Constructor2.java
@@ -0,0 +1,13 @@
+//Default Constructor
+package myPackage;
+
+public class Constructor2 {
+	public Constructor2() {
+		System.out.println("Hi I'm a Default Constructor");
+	}
+	public static void main(String[] args) {
+		Constructor2 o1 = new Constructor2();
+		Constructor2 o2 = new Constructor2();
+		Constructor2 o3 = new Constructor2();
+	}
+}
